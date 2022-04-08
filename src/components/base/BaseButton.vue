cd7e61defe3f610a3d676f07402243f51738c80b
Drew <drewclementsdesign@gmail.com>
2022-04-07 23:22:49 -0400

    back to vue 3. init set up

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,48 +1,52 @@
 <template>
-  <nuxt-link :class="`btn btn-${theme}`" v-if="href" :to="href">
+  <router-link :class="`btn btn-${theme}`" v-if="href" :to="href">
     <slot />
-  </nuxt-link>
+  </router-link>
 
-  <button v-else :class="`btn btn-${theme}`" :type="type" @click="$emit('click')">
+  <button
+    v-else
+    :class="`btn btn-${theme}`"
+    :type="type"
+    @click="$emit('click')"
+  >
     <slot />
   </button>
 </template>
 
 <script>
-  export default {
-    props: {
-      type: {
-        type: String,
-        default: 'button',
-      },
-      theme: {
-        type: String,
-        default: 'primary',
-      },
-      href: {
-        type: String,
-        required: false,
-      },
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "button",
     },
-  }
+    theme: {
+      type: String,
+      default: "primary",
+    },
+    href: {
+      type: String,
+      required: false,
+    },
+  },
+};
 </script>
 
 <style lang="postcss" scoped>
+.btn {
+  @apply px-3 py-0.5 font-monts text-center rounded border-2 border-transparent;
+}
+.btn-primary {
+  @apply bg-red-500 text-white;
+}
+
+.btn-secondary {
+  @apply border-blue-500 text-blue-500;
+}
+
+@screen lg {
   .btn {
-    @apply px-3 py-0.5 font-monts text-center rounded border-2 border-transparent;
-
-    &.btn-primary {
-      @apply bg-red-500 text-white;
-    }
-
-    &.btn-secondary {
-      @apply border-blue-500 text-blue-500;
-    }
+    @apply px-6 py-1;
   }
-
-  @screen lg {
-    .btn {
-      @apply px-6 py-1;
-    }
-  }
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 module.exports = {
+  content: ["./src/**/*.{vue,js}"],
   theme: {
     fontFamily: {
-      monts: ['Montserrat', 'Arial', 'sans-serif'],
-      body: ['Hind', 'sans-serif'],
+      monts: ["Montserrat", "Arial", "sans-serif"],
+      body: ["Hind", "sans-serif"],
     },
     extend: {
       colors: {
         red: {
-          500: '#D94B4B',
+          500: "#D94B4B",
         },
         blue: {
-          500: '#1E8AB9',
+          500: "#1E8AB9",
         },
         green: {
-          500: '#2A9F28',
+          500: "#2A9F28",
         },
-        black: '#1E1E1E',
+        black: "#1E1E1E",
       },
     },
   },
-}
+};
