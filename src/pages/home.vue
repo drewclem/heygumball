4a377196afe818f77882eb726203e6faaf0c8b71
Drew <drewclementsdesign@gmail.com>
2022-04-07 22:52:06 -0400

    vue 3 init

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,13 +3,17 @@
     <section class="base-wrapper">
       <div class="grid lg:grid-cols-2 gap-12 items-center">
         <div class="rounded-2xl overflow-hidden lg:order-last">
-          <BaseImage src="./hero_image.jpg" alt="A woman works efficiently at a laptop" />
+          <BaseImage
+            src="./hero_image.jpg"
+            alt="A woman works efficiently at a laptop"
+          />
         </div>
 
         <div class="flex flex-col space-y-6">
           <BaseHeading size="h1">You're in<br />high demand.</BaseHeading>
           <BaseText class="lg:w-3/4">
-            Take control of managing and scheduling your booking requests, easily.
+            Take control of managing and scheduling your booking requests,
+            easily.
           </BaseText>
 
           <div>
@@ -21,19 +25,21 @@
 
     <section class="narrow-wrapper my-24 lg:my-52">
       <BaseHeading size="h2" class="text-center">
-        The world's <span class="text-green-500 underline">first</span> limited window requests
-        management tool.
+        The world's <span class="text-green-500 underline">first</span> limited
+        window requests management tool.
       </BaseHeading>
 
       <div class="max-w-3xl mx-auto">
         <div class="grid lg:grid-cols-2 gap-12 items-center my-20 lg:my-48">
           <div class="flex flex-col space-y-3">
             <ScheduleGraphic class="w-1/2 lg:hidden mx-auto mb-4" />
-            <BaseHeading class="text-blue-500" size="h3">Schedule Request Windows</BaseHeading>
+            <BaseHeading class="text-blue-500" size="h3"
+              >Schedule Request Windows</BaseHeading
+            >
             <BaseText>
-              Pre-define moments for your requests form to be live and actively accepting new
-              inquiries.<br /><br />Customize the prospects form with as many, or as little, fields
-              as you need!
+              Pre-define moments for your requests form to be live and actively
+              accepting new inquiries.<br /><br />Customize the prospects form
+              with as many, or as little, fields as you need!
             </BaseText>
           </div>
           <div class="hidden lg:block">
@@ -47,11 +53,13 @@
           </div>
           <div class="flex flex-col space-y-3">
             <SearchGraphic class="w-1/2 lg:hidden mx-auto mb-4" />
-            <BaseHeading class="text-blue-500" size="h3">Search &amp; Tag your Backlog</BaseHeading>
+            <BaseHeading class="text-blue-500" size="h3"
+              >Search &amp; Tag your Backlog</BaseHeading
+            >
             <BaseText>
-              Review, search, & filter your backlog by keyword, general search, custom tags, or
-              filtered down by booking windows.<br /><br />Finding the right prospects has never
-              been easier!
+              Review, search, & filter your backlog by keyword, general search,
+              custom tags, or filtered down by booking windows.<br /><br />Finding
+              the right prospects has never been easier!
             </BaseText>
           </div>
         </div>
@@ -63,8 +71,9 @@
               >Review &amp; Respond to Submissions</BaseHeading
             >
             <BaseText>
-              Review and respond to prospects requests from the easy-to-use dashboard. Mark requests
-              as denied, approved, contacted, and booked!<br /><br />
+              Review and respond to prospects requests from the easy-to-use
+              dashboard. Mark requests as denied, approved, contacted, and
+              booked!<br /><br />
               One-click reply to those that make the cut!
             </BaseText>
           </div>
@@ -76,26 +85,40 @@
     </section>
 
     <section class="narrow-wrapper mb-20">
-      <div class="grid grid-cols-1 lg:grid-cols-2 space-y-10 lg:space-y-0 items-center">
+      <div
+        class="grid grid-cols-1 lg:grid-cols-2 space-y-10 lg:space-y-0 items-center"
+      >
         <div class="grid grid-cols-2 gap-4 lg:order-last">
-          <div class="lg:w-3/4 lg:border-l border-gray-200 lg:px-6 mb-8 lg:mb-0">
+          <div
+            class="lg:w-3/4 lg:border-l border-gray-200 lg:px-6 mb-8 lg:mb-0"
+          >
             <IconReply class="w-8 h-8 mb-4" />
-            <BaseHeading class="mb-2" size="h5" tag="h3">One-click reply</BaseHeading>
+            <BaseHeading class="mb-2" size="h5" tag="h3"
+              >One-click reply</BaseHeading
+            >
             <BaseText>Reply to requests easily with a single click.</BaseText>
           </div>
 
           <div class="lg:w-3/4 lg:border-l border-gray-200 lg:pl-6">
             <IconSorting class="w-8 h-8 mb-4" />
-            <BaseHeading class="mb-2" size="h5" tag="h3">Backlog Sorting</BaseHeading>
-            <BaseText>View requests by date, keyword, or all at once. Your pick!</BaseText>
+            <BaseHeading class="mb-2" size="h5" tag="h3"
+              >Backlog Sorting</BaseHeading
+            >
+            <BaseText
+              >View requests by date, keyword, or all at once. Your
+              pick!</BaseText
+            >
           </div>
         </div>
 
         <div class="flex flex-col space-y-8 lg:w-3/4">
           <div>
-            <BaseHeading class="mb-3" size="h4" tag="h2">Ready to get started?</BaseHeading>
+            <BaseHeading class="mb-3" size="h4" tag="h2"
+              >Ready to get started?</BaseHeading
+            >
             <BaseText>
-              Sign up today and see how much better managing your booking requests can be!
+              Sign up today and see how much better managing your booking
+              requests can be!
             </BaseText>
           </div>
 
@@ -109,20 +132,13 @@
 </template>
 
 <script>
-  import ScheduleGraphic from '~/assets/Schedule.svg?inline'
-  import SearchGraphic from '~/assets/Search.svg?inline'
-  import ReviewGraphic from '~/assets/Review.svg?inline'
-  import IconReply from '~/assets/IconReply.svg?inline'
-  import IconSorting from '~/assets/IconSorting.svg?inline'
+// import ScheduleGraphic from "./assets/Schedule.svg";
+// import SearchGraphic from "./assets/Search.svg?inline";
+// import ReviewGraphic from "./assets/Review.svg?inline";
+// import IconReply from "./assets/IconReply.svg?inline";
+// import IconSorting from "./assets/IconSorting.svg?inline";
 
-  export default {
-    name: 'Home',
-    components: {
-      IconReply,
-      IconSorting,
-      ScheduleGraphic,
-      SearchGraphic,
-      ReviewGraphic,
-    },
-  }
+export default {
+  name: "HomePage",
+};
 </script>
--- utils/registerIconComponents.js
+++
@@ -1,9 +0,0 @@
-export default {
-  AddressIcon: () => import("~/assets/icons/address-icon.svg?inline"),
-  PhoneIcon: () => import("~/assets/icons/phone-icon.svg?inline"),
-  MenuIcon: () => import("~/assets/icons/menu-icon.svg?inline"),
-  CloseIcon: () => import("~/assets/icons/close-icon.svg?inline"),
-  BookIcon: () => import("~/assets/icons/book-icon.svg?inline"),
-  PeopleIcon: () => import("~/assets/icons/people-icon.svg?inline"),
-  PersonIcon: () => import("~/assets/icons/person-icon.svg?inline"),
-};
