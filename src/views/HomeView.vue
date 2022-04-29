09e2be1e234a71d740bc3d8f08b240eeadadfbd9
Drew <drewclementsdesign@gmail.com>
2022-04-28 23:48:12 -0400

    marketing copy changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,20 +28,17 @@
 
       <section class="narrow-wrapper my-24 lg:my-52">
         <div>
-          <BaseHeading size="h2" class="lg:text-center mb-8">
-            Control
-            <span class="text-green-500 underline">when</span> and
-            <span class="text-blue-500 underline">how</span> your clients can
-            request appointments.
+          <BaseHeading size="h2" class="lg:text-center mb-8 !leading-tight">
+            Managing booking requests <br />just got
+            <span class="text-green-500">awesome</span>
           </BaseHeading>
 
-          <BaseText
-            class="text-lg lg:text-2xl max-w-4xl mx-auto lg:text-center"
-          >
-            Heygumball allows you to direct your clients to a single point of
-            contact when it comes to requesting an appointment with you. We give
-            <span class="italic font-semibold">you</span> the ability to decide
-            and schedule when that window for booking is open.
+          <BaseText class="text-lg lg:text-xl max-w-5xl mx-auto lg:text-center">
+            Everyone wants to book time with you, and for good reason! But your
+            time is best spent creating the value everyone wants. Heygumball
+            puts the power back in your hands by letting you decide when
+            you&apos;re open to take requests and by giving you tools to manage,
+            sort, and respond in a simple and efficient way!
           </BaseText>
         </div>
 
@@ -49,13 +46,15 @@
           <div class="grid lg:grid-cols-2 gap-12 items-center my-20 lg:my-48">
             <div class="flex flex-col space-y-3">
               <ScheduleGraphic class="w-1/2 lg:hidden mx-auto mb-4" />
-              <BaseHeading class="text-blue-500" size="h3"
-                >Schedule Request Windows</BaseHeading
-              >
+              <BaseHeading class="text-blue-500" size="h3">
+                Schedule when your books are open
+              </BaseHeading>
               <BaseText>
-                Pre-define moments for your requests form to be live and
-                actively accepting new inquiries.<br /><br />Customize the
-                prospects form with as many, or as little, fields as you need!
+                Do you know when you'll be booking next? Get ahead of the curve
+                by scheduling your Heygumball form to be live and open to the
+                public.<br /><br />You'll get notifications when requests start
+                coming in <span class="italic">and</span> your request form will
+                close itself when it's time has lapsed.
               </BaseText>
             </div>
             <div class="hidden lg:block">
@@ -69,13 +68,15 @@
             </div>
             <div class="flex flex-col space-y-3">
               <SearchGraphic class="w-1/2 lg:hidden mx-auto mb-4" />
-              <BaseHeading class="text-blue-500" size="h3"
-                >Search &amp; Tag your Backlog</BaseHeading
-              >
+              <BaseHeading class="text-blue-500" size="h3">
+                Search, filter, &amp; tag your backlog
+              </BaseHeading>
+
               <BaseText>
-                Review, search, & filter your backlog by keyword, general
-                search, custom tags, or filtered down by booking windows.<br /><br />Finding
-                the right prospects has never been easier!
+                Navigating your requests has never been easier! Search by
+                keyword, name, date.. you name it, we find it.<br /><br />requests
+                are grouped by booking windows by default, making it easy for
+                you to see the most recent and relevant items.
               </BaseText>
             </div>
           </div>
@@ -84,13 +85,14 @@
             <div class="flex flex-col space-y-3">
               <ReviewGraphic class="w-1/2 lg:hidden mx-auto mb-4" />
               <BaseHeading class="text-blue-500" size="h3"
-                >Review &amp; Respond to Submissions</BaseHeading
+                >Review &amp; respond to requests</BaseHeading
               >
               <BaseText>
-                Review and respond to prospects requests from the easy-to-use
-                dashboard. Mark requests as denied, approved, contacted, and
+                Review and respond to prospective requests from the easy-to-use
+                dashboard. Mark requests as denied, approved, contacted, or
                 booked!<br /><br />
-                One-click reply to those that make the cut!
+                Once you've narrowed down your options, one-click reply to those
+                that made the cut and start filling up your calendar!
               </BaseText>
             </div>
             <div class="hidden lg:block">
