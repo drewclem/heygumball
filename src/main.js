a762b9cdd406729c5bea09b116cc6a93c3da8392
Drew <drewclementsdesign@gmail.com>
2022-05-03 00:31:45 -0400

    init pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import router from "./router";
+import { createPinia } from "pinia";
 import "./assets/css/main.css";
 import App from "./App.vue";
 
@@ -7,7 +8,7 @@ import App from "./App.vue";
 import { supabase } from "./supabase";
 import { userSession } from "@/vuetils/useAuth";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
 
 supabase.auth.onAuthStateChange((event, session) => {
   userSession.value = session;
