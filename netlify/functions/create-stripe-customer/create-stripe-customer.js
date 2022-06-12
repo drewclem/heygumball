faffe87d0df5c7afd19e082ac914725fc381c785
Drew <drewclementsdesign@gmail.com>
2022-06-11 23:26:09 -0400

    get stripe customer id and store in DB

---
+++ netlify/functions/create-stripe-customer/create-stripe-customer.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(
+  process.env.VUE_APP_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY
+);
+
+exports.handler = async (req, res) => {
+  const initReq = JSON.parse(req.body);
+  try {
+    const customer = await stripe.customers.create({
+      email: initReq.record.email,
+    });
+
+    const { data, error } = await supabase
+      .from("profiles")
+      .update({
+        stripe_customer: customer.id,
+      })
+      .eq("id", initReq.record.id);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Account linked for:${customer.id}` }),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
